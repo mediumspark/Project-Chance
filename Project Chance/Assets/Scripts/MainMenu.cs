c0e8d15c195a2c079e363e9ad1d93e06c23ec755
Eddie Fulton <50469027+mediumspark@users.noreply.github.com>
2021-11-21 01:49:30 -0600

    Opening Cutscene and reJon Commit.

--- Project Chance/Assets/Scripts/MainMenu.cs
+++ Project Chance/Assets/Scripts/MainMenu.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     public string newGameScene;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void StartCutscene(GameObject go)
     {
-        
+        go.SetActive(true); 
     }
 
     public void NewGame()
